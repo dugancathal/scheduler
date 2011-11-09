dc709f57bf0abe7bd08bb491caa1e602cc0fe61b
thtaylor <thtaylor@mines.edu>
2011-11-09 13:54:23 -0700

    First commit, with readme.

---
+++ src/thread.rb
@@ -0,0 +1,14 @@
+module Simulator
+  class Thread
+    attr_accessor :arrival, :bursts, :burst_lengths
+    def initialize(arrival, bursts)
+      @arrival = arrival
+      @bursts = bursts
+      @burst_lengths = []
+    end
+
+    def add_burst(cpu_length, io_length = 0)
+      @burst_lengths << {cpu: cpu_length, io: io_length}
+    end
+  end
+end
